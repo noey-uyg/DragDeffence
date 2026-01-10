e954e32b1f792bed716f6596faf56cf58d898bfa
Noeyuyg <dnlrbdus31@gmail.com>
2026-01-10 16:15:38 +0900

    - Add : 업그레이드 뷰 줌인아웃

---
+++ Assets/Resources/Scripts/Game/UI/Upgrade/ViewZoomHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ViewZoomHandler : MonoBehaviour, IScrollHandler
+{
+    [SerializeField] private RectTransform _content;
+    [SerializeField] private float _zoomSpeed = 0.1f;
+    [SerializeField] private float _minZoom = 0.5f;
+    [SerializeField] private float _maxZoom = 1.5f;
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        Vector3 mouseWorldPos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(_content, eventData.position, eventData.pressEventCamera, out mouseWorldPos);
+
+        float zoomDelta = eventData.scrollDelta.y * _zoomSpeed;
+        Vector3 nextScale = _content.localScale + Vector3.one * zoomDelta;
+
+        nextScale.x = Mathf.Clamp(nextScale.x, _minZoom, _maxZoom);
+        nextScale.y = Mathf.Clamp(nextScale.y, _minZoom, _maxZoom);
+        nextScale.z = 1f;
+
+        _content.localScale = nextScale;
+
+        Vector3 mouseWorldPosAfterZoom;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(_content, eventData.position, eventData.pressEventCamera, out mouseWorldPosAfterZoom);
+
+        Vector3 delta = mouseWorldPos - mouseWorldPosAfterZoom;
+        _content.position -= delta;
+    }
+}
